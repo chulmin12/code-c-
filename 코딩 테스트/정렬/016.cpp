d491d06066ab53e10c74da087428304cc3625cb6
chulmin12 <gogurye97@gmail.com>
2023-10-08 21:10:53 +0900

    2023.10.08(삽입정렬 시작해야함)

---
+++ 코딩 테스트/정렬/016.cpp
@@ -0,0 +1,8 @@
+//
+//  016.cpp
+//  code_master
+//
+//  Created by Hwangchulmin on 2023/10/08.
+//
+
+#include <stdio.h>
---
+++ 코딩 테스트/정렬/018.cpp
@@ -0,0 +1,8 @@
+//
+//  018.cpp
+//  code_master
+//
+//  Created by Hwangchulmin on 2023/10/08.
+//
+
+#include <stdio.h>
