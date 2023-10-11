482a6d9e5aca0815a77285d7eb6a7286b74e5e4d
chulmin12 <gogurye97@gmail.com>
2023-10-11 20:17:10 +0900

    2023.10.11 (탐색)

---
+++ 코딩 테스트/정렬/022.cpp
@@ -0,0 +1,8 @@
+//
+//  022.cpp
+//  code_master
+//
+//  Created by Hwangchulmin on 2023/10/11.
+//
+
+#include <stdio.h>
---
+++ 코딩 테스트/탐색/023.cpp
@@ -0,0 +1,8 @@
+//
+//  023.cpp
+//  code_master
+//
+//  Created by Hwangchulmin on 2023/10/11.
+//
+
+#include <stdio.h>
